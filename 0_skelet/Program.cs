110df9402bc056b85b684fbd5d343aca43190d12
Ordur_pc <ilnur.lila0@gmail.com>
2024-09-11 12:05:19 +0300

    start

---
+++ 0_skelet/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+class Program
+{
+    static void Main()
+    {
+        //
+        
+    }
+}
---
+++ 1_Система_урона_56.58/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("123");
+    }
+}
