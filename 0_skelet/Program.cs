97626ac0e81cbc28553063cd9b11e6a9bbdacb92
Ordur_pc <ilnur.lila0@gmail.com>
2024-09-17 08:56:14 +0300

    queue_stack

--- 0_skelet/Program.cs
+++ 0_skelet/Program.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using System.Collections.Generic;
 
-
 class Program
 {
     static void Main()
---
+++ 1_Queue/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Queue<string> pat = new Queue<string>();
+
+        pat.Enqueue("Vasya");
+        pat.Enqueue("Petrovich");
+        pat.Enqueue("Bomj");
+        pat.Enqueue("Brodyaga");
+
+        foreach (string p in pat)
+        {
+            Console.WriteLine(p); 
+        }
+
+        Console.WriteLine();
+
+        Stack<string> pat1 = new Stack<string>();
+
+        pat1.Push("Vasya");
+        pat1.Push("Petrovich");
+        pat1.Push("Bomj");
+        pat1.Push("Brodyaga");
+
+        Console.WriteLine(pat1.Pop());
+
+        foreach (string p in pat1)
+        {
+            Console.WriteLine(p); 
+        }
+    }
+}
