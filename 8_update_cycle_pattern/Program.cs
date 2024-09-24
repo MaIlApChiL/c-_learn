cc52cb12c526eae55d58b1932f9b3a0ff11776e9
Ordur_pc <ilnur.lila0@gmail.com>
2024-09-24 19:00:36 +0300

    8_update_cycle_pattern

---
+++ 8_update_cycle_pattern/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Behaviour[] behaviours =
+        {
+            new Walker(),
+            new Jumper()
+        };
+
+        while (true)
+        {
+            foreach (var behaviour in behaviours)
+            {
+                behaviour.Update();
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+}
+
+class Behaviour
+{
+    public virtual void Update()
+    {
+
+    }
+}
+
+class Walker : Behaviour
+{
+    public override void Update()
+    {
+        Console.WriteLine("Иду");
+    }
+}
+
+class Jumper : Behaviour
+{
+    public override void Update()
+    {
+        Console.WriteLine("Прыгаю");
+    }
+}
