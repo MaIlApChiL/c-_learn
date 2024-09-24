fc7e71bdd86f81f3aca3fc411e7159d2f5e102ad
Ordur_pc <ilnur.lila0@gmail.com>
2024-09-24 19:34:37 +0300

    10_started

---
+++ 10_abstract_class/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Vehicle car = new Car();
+    }
+}
+
+abstract class Vehicle
+{
+    protected float Speed;
+
+    public abstract void Move();
+
+    public float GetCurrentSpeed()
+    {
+        return Speed;
+    }
+}
+
+class Car : Vehicle
+{
+    public override void Move()
+    {
+        Console.WriteLine("Машина едет по асфальту.")
+    }
+}
---
+++ 9_interfaces/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        
+    }
+}
+
+interface IMovable
+{
+    void Move();
+    void ShowMoveSpeed();
+}
+
+interface IBurnable
+{
+    void Burn();
+}
+
+class Vehicle
+{
+
+}
+
+class Car : Vehicle, IMovable, IBurnable
+{
+    public void Move()
+    {
+
+    }
+
+    public void ShowMoveSpeed()
+    {
+
+    }
+
+    public void Burn()
+    {
+
+    }
+}
+
+class Human : IMovable
+{
+    public void Move()
+    {
+
+    }
+
+    public void ShowMoveSpeed()
+    {
+
+    }
+}
